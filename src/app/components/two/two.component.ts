580ceea86af8ff4cd82f0bddc7914c51c05850c0
Harold Flores <bhflores1994@gmail.com>
2021-10-11 12:46:39 -0500

    update badge without dev depencency

--- src/app/components/two/two.component.ts
+++ src/app/components/two/two.component.ts
@@ -12,7 +12,25 @@ export class TwoComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  add(){
+  test(){
+    return true;
+  }
+  test1(){
+    return true;
+  }
+  test2(){
+    return true;
+  }
+  test3(){
+    return true;
+  }
+  test4(){
+    return true;
+  }
+  test5(){
+    return true;
+  }
+  test6(){
     return true;
   }
 }
